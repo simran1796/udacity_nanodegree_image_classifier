e2248c69b1511a20b3779d4ac1f3640c82e6b0e1
simran1796 <44257580+simran1796@users.noreply.github.com>
2020-01-14 09:49:07 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,90 @@
+import signal
+
+from contextlib import contextmanager
+
+import requests
+import argparse
+import torch
+from torchvision import transforms, datasets
+import copy
+import os
+import json
+
+DELAY = INTERVAL = 4 * 60  # interval time in seconds
+MIN_DELAY = MIN_INTERVAL = 2 * 60
+KEEPALIVE_URL = "https://nebula.udacity.com/api/v1/remote/keep-alive"
+TOKEN_URL = "http://metadata.google.internal/computeMetadata/v1/instance/attributes/keep_alive_token"
+TOKEN_HEADERS = {"Metadata-Flavor":"Google"}
+
+
+def _request_handler(headers):
+    def _handler(signum, frame):
+        requests.request("POST", KEEPALIVE_URL, headers=headers)
+    return _handler
+
+
+@contextmanager
+def active_session(delay=DELAY, interval=INTERVAL):
+    """
+    Example:
+
+    from workspace_utils import active session
+
+    with active_session():
+        # do long-running work here
+    """
+    token = requests.request("GET", TOKEN_URL, headers=TOKEN_HEADERS).text
+    headers = {'Authorization': "STAR " + token}
+    delay = max(delay, MIN_DELAY)
+    interval = max(interval, MIN_INTERVAL)
+    original_handler = signal.getsignal(signal.SIGALRM)
+    try:
+        signal.signal(signal.SIGALRM, _request_handler(headers))
+        signal.setitimer(signal.ITIMER_REAL, delay, interval)
+        yield
+    finally:
+        signal.signal(signal.SIGALRM, original_handler)
+        signal.setitimer(signal.ITIMER_REAL, 0)
+
+
+def keep_awake(iterable, delay=DELAY, interval=INTERVAL):
+    """
+    Example:
+
+    from workspace_utils import keep_awake
+
+    for i in keep_awake(range(5)):
+        # do iteration with lots of work here
+    """
+    with active_session(delay, interval): yield from iterable
+def save_checkpoint(path, model, optimizer, args, classifier):
+    
+    checkpoint = {'arch': args.arch, 
+                  'model': model,
+                  'learning_rate': args.learning_rate,
+                  'hidden_units': args.hidden_units,
+                  'classifier' : classifier,
+                  'epochs': args.epochs,
+                  'optimizer': optimizer.state_dict(),
+                  'state_dict': model.state_dict(),
+                  'class_to_idx': model.class_to_idx}
+
+    torch.save(checkpoint, path)
+    
+    
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model = checkpoint['model']
+    model.classifier = checkpoint['classifier']
+    learning_rate = checkpoint['learning_rate']
+    epochs = checkpoint['epochs']
+    optimizer = checkpoint['optimizer']
+    model.load_state_dict(checkpoint['state_dict'])
+    model.class_to_idx = checkpoint['class_to_idx']
+    
+    return model
+
+def load_cat_names(filename):
+    with open(filename) as f:
+        category_names = json.load(f)
+    return category_names
